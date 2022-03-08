17c09a71acb6234b252039a6ad0e82a72a09197a
LabiJH <97749869+LabiJH@users.noreply.github.com>
2022-03-08 19:54:12 +0100

    Add files via upload

---
+++ FriendServer/FriendServer/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FriendServer
+{
+    partial class FriendServer
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnecterWorker = new System.ComponentModel.BackgroundWorker();
+            this.ChatAbfang = new System.Windows.Forms.TextBox();
+            this.Überschrift = new System.Windows.Forms.Label();
+            this.Adresse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ConnecterWorker
+            // 
+            this.ConnecterWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.ConnecterWorker_DoWork);
+            // 
+            // ChatAbfang
+            // 
+            this.ChatAbfang.Location = new System.Drawing.Point(12, 66);
+            this.ChatAbfang.Multiline = true;
+            this.ChatAbfang.Name = "ChatAbfang";
+            this.ChatAbfang.Size = new System.Drawing.Size(390, 242);
+            this.ChatAbfang.TabIndex = 0;
+            // 
+            // Überschrift
+            // 
+            this.Überschrift.AutoSize = true;
+            this.Überschrift.Location = new System.Drawing.Point(54, 9);
+            this.Überschrift.Name = "Überschrift";
+            this.Überschrift.Size = new System.Drawing.Size(305, 13);
+            this.Überschrift.TabIndex = 1;
+            this.Überschrift.Text = "!!!Dieses Fenster ist nur zur Überwachung des Chats gedacht!!!";
+            // 
+            // Adresse
+            // 
+            this.Adresse.AutoSize = true;
+            this.Adresse.Location = new System.Drawing.Point(89, 35);
+            this.Adresse.Name = "Adresse";
+            this.Adresse.Size = new System.Drawing.Size(35, 13);
+            this.Adresse.TabIndex = 2;
+            this.Adresse.Text = "label1";
+            // 
+            // FriendServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 320);
+            this.Controls.Add(this.Adresse);
+            this.Controls.Add(this.Überschrift);
+            this.Controls.Add(this.ChatAbfang);
+            this.Name = "FriendServer";
+            this.Text = "Jener Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker ConnecterWorker;
+        private System.Windows.Forms.TextBox ChatAbfang;
+        private System.Windows.Forms.Label Überschrift;
+        private System.Windows.Forms.Label Adresse;
+    }
+}
+
---
+++ FriendServer/FriendServer/Form1.cs
@@ -0,0 +1,101 @@
+﻿/***************************************************************************************************************************************************************************
+Programm:                       FriendServer
+Funktion:                       Der Server zum FriendChat der die empfangenen Nachrichten weiterleitet
+                                an die anderen Clienten.
+Letzte Änderung:                21-06-2016
+Autor:                          Labinot Jaha
+***************************************************************************************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace FriendServer
+{
+    public partial class FriendServer : Form
+    {
+        public FriendServer()
+        {
+            InitializeComponent();
+            ConnecterWorker.RunWorkerAsync();
+        }
+
+        private void ConnecterWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            List<string> clientListe = new List<string>();                          // Schonmal verbundene Clients werden in dieser Liste abgespeichert
+            bool neuerClient;                                                       // Prüfungs Varaible um zu gucken ob der Client schonmal verbunden war
+            int schleifenCounter;                                                  // Ein Zähler für das prüfen des Clienten
+
+            UdpClient udpClient = new UdpClient(55555);                                   // Empfangs Port der Clients
+            IPEndPoint serverClientAdresse = new IPEndPoint(IPAddress.Any, 0);            // Empfangs Addresse der Clients (Nimmt die Adresse der Clienten an)
+
+            /* Ausgeben der Server Adresse sowie Port */
+            string HostName = Dns.GetHostName();                                                    // Hostname der PC
+            IPHostEntry hostInfo = Dns.GetHostByName(HostName);                                     // Auslesen der !!IPV4!! Addresse des derzeitigen Host PCs
+            string ipadresse = hostInfo.AddressList[0].ToString();                                      // Schreiben der IPv4 Addresse in eine Variable
+            string portPlatzhalter = (((IPEndPoint)udpClient.Client.LocalEndPoint).Port.ToString());    // Schreiben des Ports in eine Variable
+            Adresse.Text = "Der Server wird gehostet von: " + ipadresse + ":" + portPlatzhalter; 
+
+            /* Unendliche Schleife */
+            while (true)
+            {
+                neuerClient = false;
+                schleifenCounter = 0;
+                try                                                                                     // Der Try verhindert das Abstürzen bei einem einzigen Fehler
+                {
+                    byte[] nachrichtenEmpfaenger = udpClient.Receive(ref serverClientAdresse);          // Hier wird auf eine Nachricht eines Clienten gewartet    
+                    string nachricht = Encoding.ASCII.GetString(nachrichtenEmpfaenger);                 // Hier werden die Nachrichten des Clienten lesbar gemacht
+
+                    do
+                    {
+                        if (clientListe.Contains(serverClientAdresse.ToString()))                     // Hier wird überprüft ob der Client bereits schonmal verbunden war
+                        {
+
+                        }
+                        else
+                        {
+                            neuerClient = true;
+                            clientListe.Add(serverClientAdresse.ToString());                          // Client wird in die Liste hinzugefügt
+                        }
+                        schleifenCounter++;
+                    } while (neuerClient == false && schleifenCounter <= clientListe.Count);
+
+                    ChatAbfang.AppendText(nachricht + Environment.NewLine);                     // Abfangen der Nachricht im Server Fenster
+
+                    /* Sendeschleife */
+                    foreach (string str in clientListe)                                         // Jeder String wird in den Folgenden Array geschrieben
+                    {
+                        string[] splitArrayIPPort;                                              // Schreiben der Client Adressen in den Array mit der Foreach Schleife
+                        string clientPort;
+                        string clientIP;
+
+                        splitArrayIPPort = str.Split(':');                                          // Abtrennen von IPv4-Adresse und Port des Senders
+                        clientIP = splitArrayIPPort[0];                                                
+                        clientPort = splitArrayIPPort[1];                                              
+
+                        int port = Convert.ToInt32(clientPort);                                         // Port zum Versenden
+                        IPAddress sendeAddresse = IPAddress.Parse(clientIP);                            // IPv4 zum Versenden
+
+                        IPEndPoint sendeEndPoint = new IPEndPoint(sendeAddresse, port);                 // Adressen an die gesendet wird
+
+                        byte[] sendeArray = Encoding.ASCII.GetBytes(nachricht.ToString());                    // Codierung der Nachricht in Bytes
+                        udpClient.Send(sendeArray, sendeArray.Length, sendeEndPoint);                         // Senden der Nachricht zum jeweiligen Clienten
+                    }
+                }
+                catch (Exception)
+                {
+                    Application.Exit();                                                                 // Schließen der Applikation durch einen Error
+                }
+            }
+        }
+    }
+}
